d2529a6293362cfe53d0aac98d5371faf5782439
Eric Phillips <eric@ewpt3ch.com>
2025-05-09 22:01:33 -0600

    working text split

--- src/markdown.py
+++ src/markdown.py
@@ -1,5 +1,22 @@
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    newnodes = []
     for node in old_nodes:
-        text = node.split(delimiter)
+        if node.text_type != TextType.TEXT:
+            newnodes.append(node)
+            continue
+        texts = node.text.split(delimiter)
+        if len(texts) % 2 == 0:
+            raise ValueError('markdown not valid, tags not closed')
+        splitnodes = []
+        for i in range(len(texts)):
+            if texts[i] == '':
+                continue
+            if i % 2 == 0:
+                splitnodes.append(TextNode(texts[i], TextType.TEXT))
+            else:
+                splitnodes.append(TextNode(texts[i], text_type))
+    return(newnodes + splitnodes)
+
+
--- src/test_markdown.py
+++ src/test_markdown.py
@@ -6,6 +6,51 @@ from markdown import split_nodes_delimiter
 class Testsplitnodes(unittest.TestCase):
     def test_code(self):
         node = TextNode("This is text with a `code` block", TextType.TEXT)
-        self.assertEqual(
+        new_nodes = split_nodes_delimiter([node], '`', TextType.CODE)
+        self.assertListEqual(new_nodes,
+                             [TextNode('This is text with a ', TextType.TEXT),
+                              TextNode('code', TextType.CODE),
+                              TextNode(' block', TextType.TEXT)]
+                              )
+
+    def test_ital(self):
+        node = TextNode("This is text with a _italic_ block", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], '_', TextType.ITALIC)
+        self.assertListEqual(new_nodes,
+                             [TextNode('This is text with a ', TextType.TEXT),
+                              TextNode('italic', TextType.ITALIC),
+                              TextNode(' block', TextType.TEXT)]
+                              )
+
+    def test_bold(self):
+        node = TextNode("This is text with a **bold** block", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], '**', TextType.BOLD)
+        self.assertListEqual(new_nodes,
+                             [TextNode('This is text with a ', TextType.TEXT),
+                              TextNode('bold', TextType.BOLD),
+                              TextNode(' block', TextType.TEXT)]
+                              )
+
+    def test_all(self):
+        node = TextNode("This is text with all **bold** `code` _italic_ block", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], '**', TextType.BOLD)
+        self.assertListEqual(new_nodes,
+                             [TextNode('This is text with all ', TextType.TEXT),
+                              TextNode('bold', TextType.BOLD),
+                              TextNode(' `code` _italic_ block', TextType.TEXT)]
+                              )
+
+    def test_only(self):
+        node = TextNode("**bold**", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], '**', TextType.BOLD)
+        self.assertListEqual(new_nodes,
+                              [TextNode('bold', TextType.BOLD)]
+                              )
+
+    def test_notclosed(self):
+        node = TextNode("bold**", TextType.TEXT)
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], '**', TextType.BOLD)
+
 if __name__ == "__main__":
     unittest.main()
