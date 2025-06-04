7f57f2153e7a80c5d252bf9618fdf33a4a032793
Eric Phillips <eric@ewpt3ch.com>
2025-06-03 21:34:02 -0600

    remove print statement

--- src/test_markdown.py
+++ src/test_markdown.py
@@ -83,7 +83,6 @@ class Testsplitnodes(unittest.TestCase):
     def test_text_to_node(self):
         text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
         nodes = text_to_textnodes(text)
-        print(nodes)
         self.assertListEqual([
             TextNode("This is ", TextType.TEXT),
             TextNode("text", TextType.BOLD),
